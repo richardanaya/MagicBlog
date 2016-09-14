dbb0fc5f7067815f77358193b8da6ded6bd0f4a7
Richard <richard.anaya@gmail.com>
2016-09-14 12:22:50 -0500

    style

--- src/client/components/postEdit.js
+++ src/client/components/postEdit.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 export default function(props){
-  return (<div className="mdl-cell mdl-cell--12-col">
-    <div>Title: <input defaultValue={props.post.title} onChange={(e)=>props.onPostChange("title",e.target.value)}/></div>
-    <div><textarea defaultValue={props.post.content} onChange={(e)=>props.onPostChange("content",e.target.value)}/></div>
-    <div>
-      <button onClick={props.onPostDelete} style={{display:(props.onPostDelete!==undefined?"block":"none")}}>Delete</button>
-      <button onClick={props.onPostSave} style={{display:(props.onPostSave!==undefined?"block":"none")}}>Save</button>
-      <button onClick={props.onPostCreate} style={{display:(props.onPostCreate!==undefined?"block":"none")}}>Create</button>
+  return (<div className="PostEdit mdl-cell mdl-cell--12-col">
+  <form action="#">
+    <div className="mdl-textfield mdl-js-textfield">
+      <input className="mdl-textfield__input" type="text" id="sample1" defaultValue={props.post.title} onChange={(e)=>props.onPostChange("title",e.target.value)}/>
+      <label className="mdl-textfield__label" htmlFor="sample1">Title...</label>
     </div>
+    <br/>
+    <div className="mdl-textfield mdl-js-textfield">
+      <textarea className="mdl-textfield__input" type="text" rows= "3" id="sample5" defaultValue={props.post.content} onChange={(e)=>props.onPostChange("content",e.target.value)}></textarea>
+      <label className="mdl-textfield__label" htmlFor="sample5">My article starts here...</label>
+    </div>
+    <div>
+      <a className="mdl-button mdl-button--raised mdl-js-ripple-effect" onClick={props.onPostDelete} style={{display:(props.onPostDelete!==undefined?"inline-block":"none")}}>Delete</a>
+      <a className="mdl-button mdl-button--raised mdl-js-ripple-effect" onClick={props.onPostSave} style={{display:(props.onPostSave!==undefined?"inline-block":"none")}}>Save</a>
+      <a className="mdl-button mdl-button--raised mdl-js-ripple-effect" onClick={props.onPostCreate} style={{display:(props.onPostCreate!==undefined?"inline-block":"none")}}>Create</a>
+    </div>
+  </form>
+
   </div>)
 }
--- src/client/components/postRead.js
+++ src/client/components/postRead.js
@@ -6,11 +6,10 @@ import CommentEntryArea from './commentEntryArea'
 export default function(props){
   var date = new Date(props.post.datetime);
   var dateString = (date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
-  return (<div className="mdl-cell mdl-cell--12-col">
-      <div>{props.post.title}</div>
-      <div>{props.post.username}</div>
-      <div>{dateString}</div>
-      <div>{props.post.content}</div>
+  return (<div className="PostRead mdl-cell mdl-cell--12-col">
+      <div className="PostRead-title">{props.post.title}</div>
+      <div className="PostRead-subtitle">Posted by {props.post.username} on {dateString}</div>
+      <div className="PostRead-content">{props.post.content}</div>
       <Comment/>
       <CommentEdit/>
       <CommentEntryArea/>
